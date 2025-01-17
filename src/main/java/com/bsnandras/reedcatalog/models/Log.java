e69077c881cee9ba170406a5eaadb76dcbb82f8d
bsnAndras <bsnAndras+github@protonmail.com>
2025-01-18 00:47:47 +0100

    chore: Log entity added, connections established

---
+++ src/main/java/com/bsnandras/reedcatalog/models/Log.java
@@ -0,0 +1,24 @@
+package com.bsnandras.reedcatalog.models;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.Date;
+
+@Data
+@Builder
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+public class Log {
+    @Id
+    @Column(name = "date_time", nullable = false, unique = true, updatable = false)
+    private Date dateTime = new Date();
+
+    @Column(nullable = false)
+    private String event;
+
+    @OneToOne
+    @JoinColumn(name = "order_no", referencedColumnName = "id")
+    private Order order;
+}
