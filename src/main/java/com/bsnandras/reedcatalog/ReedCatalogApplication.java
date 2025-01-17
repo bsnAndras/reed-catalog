ed58ed0b50abb372283439f610eb89a7d879b09c
bsnAndras <bsnAndras+github@protonmail.com>
2025-01-17 21:25:21 +0100

    chore: Reed Catalog project setup Done!

---
+++ src/main/java/com/bsnandras/reedcatalog/ReedCatalogApplication.java
@@ -0,0 +1,13 @@
+package com.bsnandras.reedcatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReedCatalogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReedCatalogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bsnandras/reedcatalog/ReedCatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bsnandras.reedcatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReedCatalogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
