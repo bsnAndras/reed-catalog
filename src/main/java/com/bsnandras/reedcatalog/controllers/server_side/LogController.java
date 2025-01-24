a7f56e1c721c8fdc3f09325751a2e3dddac14800
bsnAndras <bsnAndras+github@protonmail.com>
2025-01-24 01:41:55 +0100

    feat: dynamic listing of the full log is done

--- src/main/java/com/bsnandras/reedcatalog/controllers/server_side/LogController.java
+++ src/main/java/com/bsnandras/reedcatalog/controllers/server_side/LogController.java
@@ -1,14 +1,12 @@
 package com.bsnandras.reedcatalog.controllers.server_side;
 
 import com.bsnandras.reedcatalog.models.Log;
-import com.bsnandras.reedcatalog.models.Order;
 import com.bsnandras.reedcatalog.repositories.LogRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import java.util.Date;
 import java.util.List;
 
 @Controller
