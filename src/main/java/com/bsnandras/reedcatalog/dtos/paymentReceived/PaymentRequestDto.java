2e77e6ae36fba458941935dc82523114a81c78f4
bsnAndras <bsnAndras+github@protonmail.com>
2025-03-20 01:50:06 +0100

    refactor: API order payment endpoint relocated to api/order/new-payment
    response added, but only with a simple String, improvement needed in the future

--- src/main/java/com/bsnandras/reedcatalog/dtos/paymentReceived/PaymentRequestDto.java
+++ src/main/java/com/bsnandras/reedcatalog/dtos/paymentReceived/PaymentRequestDto.java
@@ -1,12 +1,21 @@
 package com.bsnandras.reedcatalog.dtos.paymentReceived;
 
 import jakarta.validation.constraints.Positive;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.util.Date;
 
 public record PaymentRequestDto(
         @Positive
         Long orderId,
+
         @Positive
-        int paymentAmount
+        int paymentAmount,
+
+        @DateTimeFormat(pattern = "yyyy-MM-dd")
+        Date transactionDate,
+
+        String note
 ) {
 
 }
