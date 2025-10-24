37e91a9521e95459f52e33f86947e525d188999a
András Földesi <bsnandras.dev+github@protonmail.com>
2025-10-25 01:15:59 +0200

    chore: setup biome, remove eslint

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,9 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+//biome-ignore-all lint: temp file, no need to lint
+//biome-ignore-all assist/source/organizeImports: temp file, no need to assist
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+
 
 function App() {
   const [count, setCount] = useState(0)
