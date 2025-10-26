e1a5736de75253f2a7ce79d9c960164e2a989adc
András Földesi <bsnandras.dev+github@protonmail.com>
2025-10-26 16:55:32 +0100

    lint: update biome settings and styling config, run biome check --write
    - .gitattributes: set up eol: LF
    (- set up vscode indentation settings)
    - .gitignore update: .vscode
    - biome ignore:
        - main.tsx: non-null assertion for finding #root element
        - vite.config.ts: exclude from format rules, to allow multiline array formatting
    
    - add biome:write shorthand npm run alias

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,11 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import tailwindcss from '@tailwindcss/vite'
-
+// biome-ignore format: config file, stay multiline
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from "vite";
 // https://vite.dev/config/
+
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss()
-  ]
-})
+  ],
+});
