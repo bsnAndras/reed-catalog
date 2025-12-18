04fa863fd7973622f258e79cebb2d01bf4f6c8c1
András Földesi <bsnandras.dev+github@protonmail.com>
2025-12-18 01:03:45 +0100

    chore: update basic layout

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,7 +8,9 @@ function App() {
   return (
     <>
       <Navbar />
-      <Outlet />
+      <main>
+        <Outlet />
+      </main>
     </>
   );
 }
--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,16 +1,18 @@
 const Navbar = () => {
   return (
-    <header>
-      <nav className="navbar bg-base-100 shadow-md mb-4">
-        <div className="flex-20 mx-2">
-          <a href="/" className="btn btn-ghost normal-case text-xl">
-            Reed Catalog
+    <header className="bg-amber-950">
+      <nav className="navbar m-auto max-w-210 xl:max-w-3/5 shadow-sm">
+        <div className="flex-1 sm:max-h-16 h-12 w-24 mx-2">
+          <a href="/">
+            <img className="h-full" src="/src/assets/logo.jpg" alt="logo" />
           </a>
-          <ul className="navlinks-container flex gap-2">
-            <li className="btn navlink">
+        </div>
+        <div className="flex-none">
+          <ul className="navlinks-container menu menu-horizontal text-amber-100 font-bold text-md">
+            <li>
               <a href="/">Dashboard</a>
             </li>
-            <li className="btn navlink">
+            <li>
               <a href="/partners">Partners</a>
             </li>
           </ul>
