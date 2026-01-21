8acad5fc8a84ebddd6fb7e16e38fde458db4ef86
András Földesi <bsnandras.dev+github@protonmail.com>
2026-01-21 17:23:44 +0100

    chore: add favicon, reformat code

--- frontend/src/components/pages/Dashboard.tsx
+++ frontend/src/components/pages/Dashboard.tsx
@@ -182,7 +182,9 @@ export const Dashboard = () => {
                   </td>
                   <td className="p-2 text-left">{log.event || "<N/A>"}</td>
                   <td className="p-2 text-right">{log.orderId ?? "-"}</td>
-                  <td className="p-2 text-right">{log.moneyExchanged ?? "0"}</td>
+                  <td className="p-2 text-right">
+                    {log.moneyExchanged ?? "0"}
+                  </td>
                   <td className="p-2 text-left">{log.partner?.name || "-"}</td>
                 </tr>
               ))
