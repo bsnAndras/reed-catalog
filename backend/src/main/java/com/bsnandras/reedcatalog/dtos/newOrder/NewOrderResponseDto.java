cc4b03307e50e6da729fa486bdfa7041136ddbf7
András Földesi <bsnandras.dev+github@protonmail.com>
2026-01-25 20:13:12 +0100

    chore: add mapping method to dtos

--- backend/src/main/java/com/bsnandras/reedcatalog/dtos/newOrder/NewOrderResponseDto.java
+++ backend/src/main/java/com/bsnandras/reedcatalog/dtos/newOrder/NewOrderResponseDto.java
@@ -6,4 +6,8 @@ public record NewOrderResponseDto(
         String message,
         Order order
 ) {
+
+    public static NewOrderResponseDto fromOrder(Order order) {
+        return new NewOrderResponseDto(String.format("New order placed: %s", order.getNotes()), order);
+    }
 }
--- backend/src/main/java/com/bsnandras/reedcatalog/dtos/paymentReceived/PaymentResponseDto.java
+++ backend/src/main/java/com/bsnandras/reedcatalog/dtos/paymentReceived/PaymentResponseDto.java
@@ -11,4 +11,15 @@ public record PaymentResponseDto(
         int newPartnerBalance,
         int moneyPaid
 ) {
+
+    public static PaymentResponseDto fromOrder(PaymentRequestDto requestDto, Order updatedOrder, int newPartnerBalance, int moneyPaid) {
+        String message = "Payment of " + moneyPaid + " received for Order ID: " + updatedOrder.getId();
+        return PaymentResponseDto.builder()
+                .message(message)
+                .request(requestDto)
+                .updatedOrder(updatedOrder)
+                .newPartnerBalance(newPartnerBalance)
+                .moneyPaid(moneyPaid)
+                .build();
+    }
 }
